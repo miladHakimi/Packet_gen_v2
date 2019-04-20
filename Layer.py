31d7deb521dc18710cb7d865ff37fe6583455f26
miladHakimi <miladhakimi33@gmail.com>
2019-04-20 23:54:01 +0430

    layer added.

---
+++ Layer.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod
+
+class Layer:
+    def __init__(self, input_count, output_count, PEs, MEMs, pe_per_leaf, leaf_count):
+        self.input_count = input_count
+        self.output_count = output_count
+        self.PEs = PEs
+        self.MEMs = MEMs
+        self.pe_per_leaf = pe_per_leaf
+        self.leaf_count = leaf_count
+    
+    @abstractmethod
+    def assign_inputs(self):
+        pass
+    
+    @abstractmethod
+    def generate_packets(self):
+        pass    
